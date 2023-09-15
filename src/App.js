1488784b3822ba330221cd45fcf3eaacdc58829f
ARJUN-TUDU <64701428+ARJUN-TUDU@users.noreply.github.com>
2023-09-15 14:16:58 +0530

    git commiting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios' 
+import {useState,useEffect} from 'react'
 
 function App() {
+
+  const [data,setData] = useState([]);
+
+  
+
+
+
+  useEffect(()=>{
+       
+    const getData = async() => {
+       
+      try {
+          const response = await axios.get("https://vercel.com/arjun-tudu/vercel-deploy-3");
+          console.log(response)
+          setData(response.data);
+      }catch(err){
+        if(err){
+          console.log("data getting error");
+        }
+      }
+
+    }
+
+
+    getData();
+     
+
+  },[])
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1>dw</h1>
+       {
+        data.map((x)=>{
+          return <h1 key = {x.age}>{x.name}</h1>
+        })
+       }
+       <hr/>
+       
+
+
+
+
+        
+     
     </div>
   );
 }
